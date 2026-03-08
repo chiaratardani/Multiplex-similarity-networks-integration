8e1e93c6e34c7a4f6b3a83a23af77447fdc8bdb2
chiaratardani <tardani.2085445@studenti.uniroma1.it>
2026-03-08 12:48:12 +0100

    Update __init__.py

--- multiplex_similarity_networks_integration/__init__.py
+++ multiplex_similarity_networks_integration/__init__.py
@@ -1,1 +1,15 @@
+from .aggregation import (
+    WeightedMeanAggregator,
+    GeometricAggregator,
+    WassersteinAggregator,
+    SNFAggregator,
+    aggregate
+)
 
+__all__ = [
+    'WeightedMeanAggregator',
+    'GeometricAggregator',
+    'WassersteinAggregator',
+    'SNFAggregator',
+    'aggregate'
+]
